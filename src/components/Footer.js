b8943f98980eacbb5ae9fb50a6d8418a5d1d2a7e
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-12-08 18:33:17 -0600

    Made the text of the footer center, made a small change to the size of the image on the home page to look better on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-function Footer(){
-    return(
-        <footer>
-<nav className="navbar" style={{display: "flex ", bottom: 0, backgroundColor: "#252525", color: "#A0A0A0"}}>
-  <span className="navbar-text" style={{textAlign: "center"}}>
-  ©2020 Zach R. Smelcer
-  </span>
-</nav>
-</footer>
-    )
-};
+function Footer() {
+  return (
+    <footer>
+      <nav
+        className="navbar justify-content-center"
+        style={{
+          display: "flex ",
+          bottom: 0,
+          backgroundColor: "#252525",
+          color: "#A0A0A0",
+        }}
+      >
+        <span className="navbar-text">©2020 Zach R. Smelcer</span>
+      </nav>
+    </footer>
+  );
+}
 
 export default Footer;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,25 +1,53 @@
 import React from "react";
-import myPic from "./assets/zachPic.png"
-
+import myPic from "./assets/zachPic.png";
 
 function Home() {
-  
   return (
     <div>
-    <div className="card"  style={{height: "80%", border: 'black', borderRadius: '0'}}>
-    <h2 className="card-header" style={{backgroundColor: '#252525', color: "#B8B8B8"}}>About Me</h2>
-    <div className="card-body" style={{backgroundColor: "#DCDCDC"}}>
-    
-    <img src={myPic} align="left" style={{margin: 5, borderRadius: "3%"}} alt="myPic"/>
-      <p className="card-text" align="right" style={{ fontSize: 18, color: "#393e46"}}>
-          My name is Zach Smelcer and I am a Junior Web-Developer currently studing at Northwestern University's Coding Boot Camp. My hope is to bring my expertise to the table towards a career doing what I love. Most of my life has revolved around technology ever since I was very little. When I would see new technologies I've never seen before, I'd be obssessed with learning about them. Growing up, I was tought a bit about how computers work and their hardware inspiring me to create my own. Recently, I've learned how to work with the brains of the computer which opened my eyes to a whole new digital world.  
-      </p>
-      <p align="right" style={{ fontSize: 18}}>
-           In high school, engineering was my favorite subject and took classes three of the four years of my high school career. These were the days that I began to realise my knack for problem solving and innovation. Now, I'm progressively learning how to innovate, test, design, and create web applications and it's been nothing but fun. There are so many wonders out there that I want to see the backbones of so I can learn how to create new things and solve new problems.
-      </p>
+      <div
+        className="card"
+        style={{ height: "80%", border: "black", borderRadius: "0" }}
+      >
+        <h2
+          className="card-header"
+          style={{ backgroundColor: "#252525", color: "#B8B8B8" }}
+        >
+          About Me
+        </h2>
+        <div className="card-body" style={{ backgroundColor: "#DCDCDC" }}>
+          <img
+            src={myPic}
+            align="left"
+            style={{ margin: 5, borderRadius: "3%", width: 250 }}
+            alt="myPic"
+          />
+          <p
+            className="card-text"
+            align="right"
+            style={{ fontSize: 18, color: "#393e46" }}
+          >
+            My name is Zach Smelcer and I am a Junior Web-Developer currently
+            studing at Northwestern University's Coding Boot Camp. My hope is to
+            bring my expertise to the table towards a career doing what I love.
+            Most of my life has revolved around technology ever since I was very
+            little. When I would see new technologies I've never seen before,
+            I'd be obssessed with learning about them. Growing up, I was tought
+            a bit about how computers work and their hardware inspiring me to
+            create my own. Recently, I've learned how to work with the brains of
+            the computer which opened my eyes to a whole new digital world.
+          </p>
+          <p align="right" style={{ fontSize: 18 }}>
+            In high school, engineering was my favorite subject and took classes
+            three of the four years of my high school career. These were the
+            days that I began to realise my knack for problem solving and
+            innovation. Now, I'm progressively learning how to innovate, test,
+            design, and create web applications and it's been nothing but fun.
+            There are so many wonders out there that I want to see the backbones
+            of so I can learn how to create new things and solve new problems.
+          </p>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
   );
 }
 
